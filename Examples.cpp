5a07e215ecb859e1c8870b5be7ebdea0b49f861e
Asaulyak Serhii <serhiomakafon@gmail.com>
2022-02-02 10:42:53 +0200

    Create new branch

--- Examples.cpp
+++ Examples.cpp
@@ -3,15 +3,23 @@
 #include <cmath>
 using namespace std;     
 
-int R(int a){
-	if (a < 1)
-		return 0;
+int R(int a) {
+	if (a <= 1)
+	return 0;
 	else(a--);
-	cout << a << endl;
-	return R(a);
+	cout << a;
+    return R(a);
+}
+int function(int b) {
+	cout << b;
+	return b;
 }
  int main()
 {
 	 R(9);
+	 function(10);
 	 
 }
+
+
+
