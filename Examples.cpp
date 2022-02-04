f6729dd1cad7358adc27cf93b3f9638f0880ad33
Asaulyak Serhii <serhiomakafon@gmail.com>
2022-02-04 15:03:19 +0200

    Simple Calculator

--- Examples.cpp
+++ Examples.cpp
@@ -3,15 +3,41 @@
 #include <cmath>
 using namespace std;     
 
-int R(int a){
-	if (a < 1)
-		return 0;
-	else(a--);
-	cout << a << endl;
-	return R(a);
-}
- int main()
+
+int main()
 {
-	 R(9);
-	 
+	setlocale(LC_ALL, "Russian");
+	double a, b;
+	char sign;
+	cout << " Enter  first number:\n";
+	cin >> a;
+	cout << " Enter  sign for solution: (example:+, -, *, /)\n";
+	cin >> sign;
+	cout << " Enter  second number:\n";
+	cin >> b;
+	
+	
+	switch (sign)
+	{
+	case '+':
+		cout << " Result of addition:\n" << a + b;
+		break;
+	case '-':
+		cout << " Result of deduction:\n" << a - b;
+		break;
+	case '*':
+		cout << " Result of multiplication:\n" << a * b;
+		break;
+	case '/':
+		if (b != 0)
+			cout << " Result of division:\n" << a / b;
+		else
+			cout << " ERROR:Division on zero!\n";
+		break;
+
+	default:
+		cout << " Incorrect sign\n";
+		break;
+	}
+			
 }
