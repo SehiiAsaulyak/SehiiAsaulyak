65d8f39fa60b23dd879bf247b2caa2cd1d38cf36
Asaulyak Serhii <serhiomakafon@gmail.com>
2022-02-22 18:07:10 +0200

    Simple psevdorandom matrix

--- Examples.cpp
+++ Examples.cpp
@@ -4,32 +4,19 @@
 #include <cmath>
 #include <ctime>
 using namespace std;     
-
+int Rows = 5;
+int Cols = 5;
+int arr[5][5];
 int main()
 {
-	int num = 0;
-		 int64_t result = 1;
-		 const int size = 11;
-		 int arr[size];
-		 int64_t fact = 1;
-
-	for (int i = 1; i < size; i++){
-			 result = result * i;
-			 arr[i] = result;
-	}
-		for (int j = 0; j < size; ++j){
-			if (j == 0){
-			  cout << j << "-" << 0 << endl;
-					 j++;
-			}
-			  cout << j <<"-" << arr[j] << endl;
-		}
-
-cout << "Enter your number (0-20): " << endl;
-			 cin >> num;
-			 for (int i = 1; i <= num; i++) {
-				 fact = fact * i;
-			 }
-			 cout << "The factorial of number " << num << " is: " << fact <<endl;
-	
+		 for (int i = 0; i < Rows; i++) {
+			 for (int j = 0; j < Cols; j++)
+				 arr[i][j] = rand() % 5 ;
+		 }
+		 cout << endl << "Pseudorandom matrix:" << endl;
+		   for (int i = 0; i < Rows; i++) {
+			   for (int j = 0; j < Cols; j++)
+				 cout << arr[i][j] << " ";
+				 cout << endl;
+		   }		  
 }
