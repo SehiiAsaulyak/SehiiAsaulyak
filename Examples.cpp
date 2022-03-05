206e4c2b33270b607ba5247104a6cf1573718915
Asaulyak Serhii <serhiomakafon@gmail.com>
2022-03-05 12:00:47 +0200

    A function that swaps the values ​​of any data types

--- Examples.cpp
+++ Examples.cpp
@@ -3,38 +3,27 @@
 #include <windows.h>
 #include <cmath>
 #include <ctime>
+#include<string>
 using namespace std;     
 
-struct Participant {
-	int id;
-	char name[10] = "";
-	char secondName [15] = "";
-	int age;
-	double weight;
-	int birthYear;
-	char education [11] = "";
+template <typename T>
+void changeValue(T& a, T& b) {
+	T temp = a;
+	a = b;
+	b = temp;
 };
-void printInformation(Participant info) {
-	cout << "Participant number:" << info.id << endl;
-	cout << "Name:" << info.name<< endl;
-	cout << "Second name:" << info.secondName<< endl;
-	cout << "Age:" << info.age<< endl;
-	cout << "Weight:" << info.weight<< endl;
-	cout << "Year of birth:" << info.birthYear<< endl;
-	cout << "Education:" << info.education << endl;
-	cout << endl;
-}
 
 int main()
 {	
-	Participant Sergey = {1,"Sergey","Asauliak",32,79.5,1989,"bachelor"};
-	Participant Ivan = {2,"Ivan","Kostopravov",44,98.3,1977,"secondary"};
-	Participant Aleksey = {3,"Aleksey","Petrov",20,69.4,2001,"master"};
+	//double var1 = 5.6;
+	//double var2 = 1.4;
+	string var1 = "hello";
+	string var2 = "goodbye";
+	cout << "a= " << var1 << endl << "b= " << var2 << endl;
 
-	printInformation(Sergey);
-	printInformation(Ivan);
-	printInformation(Aleksey);
+	cout << "Swap:" << endl;
 
-	
+	changeValue(var1, var2);
+	cout << "a= " << var1 << endl << "b= " << var2 << endl;
 }
 
