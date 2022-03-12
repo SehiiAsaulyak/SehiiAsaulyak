b1becaf0edbbacc38dd0f06e4c9577c9e1661bf4
Asaulyak Serhii <serhiomakafon@gmail.com>
2022-03-12 11:53:50 +0200

    Own strlen function

--- Examples.cpp
+++ Examples.cpp
@@ -1,29 +1,23 @@
 ﻿#define _USE_MATH_DEFINES
 #include <iostream>
 #include <windows.h>
-#include <cmath>
 #include <ctime>
 #include<string>
 using namespace std;     
 
-template <typename T>
-void changeValue(T& a, T& b) {
-	T temp = a;
-	a = b;
-	b = temp;
+int strLen(const char* string) {
+	int index = 0;
+	while (string[index] != '\0') {
+		index++;
+	}
+	cout << index << endl;
+	return index;
 };
 
 int main()
-{	
-	//double var1 = 5.6;
-	//double var2 = 1.4;
-	string var1 = "hello";
-	string var2 = "goodbye";
-	cout << "a= " << var1 << endl << "b= " << var2 << endl;
+{
+	strLen("");
+	strLen("Hello world!");	
 
-	cout << "Swap:" << endl;
-
-	changeValue(var1, var2);
-	cout << "a= " << var1 << endl << "b= " << var2 << endl;
 }
 
